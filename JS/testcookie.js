af21f33177589b304f5436a08f950a483e347018
Varask <guillaume.benhamou@etu.estia.fr>
2024-04-17 11:27:06 +0200

    Update CSS and HTML files

--- JS/testcookie.js
+++
@@ -1,9 +0,0 @@
-import  { CookieHandler }  from  './cookieHandler.js' ;
-
-ch = CookieHandler();
-
-ch.generateCookie("test", "test");
-ch.generateCookie("connexionID", "1234567890");
-ch.printCookies();
-ch.send_to_printer_div();
-
