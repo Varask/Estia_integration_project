645f138b31de3e459f891d6dc110a53e2b566d8b
Lucas Brana <l.brana@net.estia.fr>
2024-04-30 14:39:47 +0200

    Fix error message in register.php

--- PHP_commands/register.php
+++ PHP_commands/register.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Vérifier si les mots de passe correspondent
     if ($password != $password2) {
-        echo "<script>alert('Les mots de passe ne correspondent pas'); window.location.href='login.html';</script>";
+        echo "<script>alert('Les mots de passe ne correspondent pas'); window.location.href='../pages/login.html';</script>";
         exit;
     }
 
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        echo "<script>alert('L\'e-mail existe déjà dans la base de données'); window.location.href='login.html';</script>";
+        echo "<script>alert('L\'e-mail existe déjà dans la base de données'); window.location.href='../pages/login.html';</script>";
         exit;
     }
 
@@ -45,15 +45,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql_security = "INSERT INTO security (password) VALUES ('$hashed_password')";
 
     if ($conn->query($sql_employee) === TRUE && $conn->query($sql_security) === TRUE) {
-        echo "<script>alert('Nouveau compte créé avec succès');</script>";
+        echo "<script>alert('Nouveau compte créé avec succès'); window.location.href='../pages/login.html';</script>";
     } else {
-        echo "<script>alert('Erreur lors de la création du compte : " . addslashes($conn->error) . "'); window.location.href='login.html';</script>";
+        echo "<script>alert('Erreur lors de la création du compte : " . addslashes($conn->error) . "'); window.location.href='../pages/login.html';</script>";
     }
 
     // Fermer la connexion
     $conn->close();
 } else {
     // Rediriger vers la page de connexion si le formulaire n'a pas été soumis
-    header("Location: login.html");
+    header("Location: ../pages/login.html");
     exit;
 }
