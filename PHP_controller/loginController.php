5bee85dcbe1d8ce23e443e504ea5bc4e3dd753d8
Lucas Brana <l.brana@net.estia.fr>
2024-05-13 11:46:12 +0200

    Fixing the loginController.php file

--- PHP_controller/loginController.php
+++ PHP_controller/loginController.php
@@ -1,47 +1,18 @@
 <?php
+include_once('../PHP_model/loginModel.php');
+
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
     $email = $_POST['email'];
     $password = $_POST['password'];
-
-    // Connexion à la base de données
-    $servername = "localhost"; // Adresse du serveur MySQL
-    $username = "root"; // Identifiant MySQL
-    $password_db = ""; // Mot de passe MySQL
-    $dbname = "dbb_integration"; // Nom de la base de données
-
-    // Création de la connexion
-    $conn = new mysqli($servername, $username, $password_db, $dbname);
-
-    // Vérifier la connexion
-    if ($conn->connect_error) {
-        die("Connexion échouée : " . $conn->connect_error);
-    }
-
-    // Préparer la requête SQL pour récupérer le mot de passe crypté
-    $sql = "SELECT e.mail, s.password 
-        FROM employee e 
-        INNER JOIN security s ON e.id = s.id_employee 
-        WHERE e.mail = '$email'";
-    $result = $conn->query($sql);
+    $result = attemptLogin($email, $password);
 
     // Vérifier s'il y a des résultats
-    if ($result->num_rows > 0) {
-        // Récupérer le mot de passe crypté depuis la base de données
-        $row = $result->fetch_assoc();
-        $hashed_password = $row['password'];
-
-        // Vérifier si le mot de passe correspond en utilisant password_verify()
-        if (password_verify($password, $hashed_password)) {
-            // Les informations de connexion sont correctes, rediriger vers la page d'accueil
-            header("Location: ../view/homepageView.html");
-            exit;
-        } else {
-            // Les informations de connexion sont incorrectes, rediriger vers la page de connexion avec un message d'erreur
-            echo "<script>alert('E-mail ou mot de passe incorrect'); window.location.href='../view/loginView.html';</script>";
-            exit;
-        }
+    if ($result) {
+        // Les informations de connexion sont correctes, rediriger vers la page d'accueil
+        header("Location: ../view/homepageView.html");
+        exit;
     } else {
         // Les informations de connexion sont incorrectes, rediriger vers la page de connexion avec un message d'erreur
         echo "<script>alert('E-mail ou mot de passe incorrect'); window.location.href='../view/loginView.html';</script>";
